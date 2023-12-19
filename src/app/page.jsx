84d728844ad4cf8b344a2e07073d08dc6f2916d4
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-12-19 11:44:43 +0600

    added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,15 @@
+import Footer from "@/components/Home/Fotter";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-
 
 export default function Home() {
   return (
-   <MaxWidthWrapper className=''>
-     <div className=" py-20 mx-auto text-center  flex flex-col items-center max-w-3xl">
+    <MaxWidthWrapper className="">
+      <div className="">
         <h2>Hello World</h2>
-        
+
         {/* All the home page section here */}
-     </div>
-   </MaxWidthWrapper>
-  )
+        <Footer></Footer>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
