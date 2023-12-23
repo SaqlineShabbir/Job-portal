ad3427232bf44efcaf11d572943d08a3364f2027
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-12-23 19:05:30 +0600

    done resume design

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,7 +6,10 @@ import { RiDeleteBinLine } from "react-icons/ri";
 const Page = () => {
   return (
     <div className="lg:px-52">
-      <button>Download Resume</button>
+      <h1 className="text-5xl text-center">Resume</h1>
+      <button className="border mt-10 w-full font-semibold text-blue-500 border-blue-400 px-10 py-5">
+        Download Resume
+      </button>
       <div className="border border-slate-100 px-[72px] py-[48px]">
         {/* Resume Header Start */}
         <div className="flex justify-between border-b pb-6">
@@ -91,7 +94,9 @@ const Page = () => {
         {/* Responsibility section start */}
         <div className="flex flex-row mt-6 border-b pb-6 text-gray-500">
           <div className="basis-3/12">
-            <p className=" ">POSITIONS OF RESPONSIBILITY </p>
+            <p className=" ">
+              POSITIONS OF <br /> RESPONSIBILITY{" "}
+            </p>
           </div>
           <div className="basis-9/12 flex justify-between">
             <div className="w-3/4">
@@ -99,7 +104,7 @@ const Page = () => {
 
               <div className="flex">
                 <button className="flex font-semibold text-blue-600 justify-center align-middle my-3">
-                  <FaPlus className="mt-1 me-2" /> Add position of <br />
+                  <FaPlus className="mt-1 me-2" /> Add position of
                   responsibility
                 </button>
               </div>
@@ -115,12 +120,98 @@ const Page = () => {
           </div>
         </div>
 
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        {/* Training and Course */}
+        <div className="flex flex-row mt-6 border-b pb-6 text-gray-500">
+          <div className="basis-3/12">
+            <p className=" "> TRAININGS/ COURSES </p>
+          </div>
+          <div className="basis-9/12 flex justify-between">
+            <div className="w-3/4">
+              <h1 className="text-black font-bold">
+                Complete Web Development Course Restart: 6
+              </h1>
+              <p>Programming Hero, Online</p>
+              <p>Aug 2023 - Present</p>
+              <p>
+                The course excels due to its comprehensive MERN stack
+                curriculum, hands-on projects, clear explanations, interactive
+                coding, real-world applicability, updated content, engaging
+                instructors, supportive community, flexible formats, and
+                potential certification. and the best course I have ever seen
+              </p>
+              <div className="flex">
+                <button className="flex font-semibold text-blue-600 justify-center align-middle my-3">
+                  <FaPlus className="mt-1 me-2" /> Add Traininh/Course
+                </button>
+              </div>
+            </div>
+            <div className="text-lg">
+              <button className="me-5 ">
+                <FiEdit2 />
+              </button>
+              <button>
+                <RiDeleteBinLine />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Acompistiment additional details */}
+        <div className="flex flex-row mt-6 border-b pb-6 text-gray-500">
+          <div className="basis-3/12">
+            <p className=" ">
+              ACCOMPLISHMENTS/ <br />
+              ADDITIONAL DETAILS{" "}
+            </p>
+          </div>
+          <div className="basis-9/12 flex justify-between">
+            <div className="w-3/4">
+              <h1 className="text-black font-bold">Job Portalr</h1>
+              <p>CeMRD, Virtual</p>
+              <p> Job • Oct 2023 - Present (3 months) </p>
+              <p>This is A nice project there form i Learn lot of.</p>
+              <div className="flex">
+                <button className="flex font-semibold text-blue-600 justify-center align-middle my-3">
+                  <FaPlus className="mt-1 me-2" /> Add accomplishment/
+                  additional detail
+                </button>
+              </div>
+            </div>
+            <div className="text-lg">
+              <button className="me-5 ">
+                <FiEdit2 />
+              </button>
+              <button>
+                <RiDeleteBinLine />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Skills */}
+
+        <div className="flex flex-row mt-6 border-b pb-6 text-gray-500">
+          <div className="basis-3/12">
+            <p className=" ">Skills</p>
+          </div>
+          <div className="basis-9/12 flex justify-between">
+            <div className="">
+              <h1 className="text-black font-bold">Skills</h1>
+              <p>Level</p>
+              <button className="flex font-semibold text-blue-600 justify-center align-middle my-3">
+                <FaPlus className="mt-1 me-2" /> Add Skills
+              </button>
+            </div>
+            <div className="text-lg">
+              <button className="me-5 ">
+                <FiEdit2 />
+              </button>
+              <button>
+                <RiDeleteBinLine />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
