5e3f466047328a6e2cf2be8f7f40e4f4188bad84
SaqlineShabbir <saqline5869@gmail.com>
2024-01-19 14:59:30 +0600

    changes

--- src/utils/apiCalls/getJobs.js
+++ src/utils/apiCalls/getJobs.js
@@ -1,6 +1,7 @@
 export default async function () {
 
-    const result = await fetch('https://job-portall.onrender.com/api/v1/jobs?')
+    // const result = await fetch('https://job-portall.onrender.com/api/v1/jobs?')
+    const result = await fetch('http://localhost:5000/api/v1/jobs?')
 
     return result.json()
 
--- src/utils/axios/axiosInstance.js
+++ src/utils/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: 'https://job-portall.onrender.com/api/v1/',
+    baseURL: 'http://localhost:5000/api/v1/',
     timeout: 1000,
     // headers: { 'X-Custom-Header': 'foobar' }
 });
