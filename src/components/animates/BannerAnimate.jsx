5c6f7d4308ae9a074bafc125ae919ae88fe10c21
SaqlineShabbir <saqline5869@gmail.com>
2024-09-11 18:48:14 +0600

    error fixed

--- src/components/animates/BannerAnimate.jsx
+++ src/components/animates/BannerAnimate.jsx
@@ -1,18 +1,15 @@
-"use client"
+"use client";
+import dynamic from "next/dynamic";
 
-import React from 'react';
-import Lottie from "lottie-react";
-import JobAnimate from '../../assets/animate-files/banner-job.json';
+// Dynamically import Lottie with server-side rendering disabled
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+import JobAnimate from "../../assets/animate-files/banner-job.json";
 
 const BannerAnimate = () => {
-    return (
-        <Lottie
-            animationData={JobAnimate}
-            height={500}
-            width={500}
-            loop={true}
-        />
-    )
-}
+  return (
+    <Lottie animationData={JobAnimate} height={500} width={500} loop={true} />
+  );
+};
 
 export default BannerAnimate;
